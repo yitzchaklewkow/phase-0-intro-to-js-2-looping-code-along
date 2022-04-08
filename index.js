14a7a4deb408c084ce06ba760402a23bbac52866
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-04-07 22:54:51 -0600

    commit tree 6d12d18ea6211cbe0e57a8fd78d626606f1ca78c

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birhtday to me!`);
+}
+*/
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i =0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+  
+  return gifts;
+}
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+countDown(10);
+
+//wrapGifts(gifts);
+
+console.log(writeCards(["Man1", "Man2", "Woman1", "Woman2"], "wedding"));
+
